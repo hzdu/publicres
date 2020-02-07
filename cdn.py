3e114d48a7f4d53b57b3416e07767bdede16a5cb
DuZhiBo <iado@foxmail.com>
2020-02-07 19:15:43 +0800

    no message

--- cdn.py
+++ cdn.py
@@ -2,17 +2,19 @@
 
 import os,sys,getopt
 
+# 默认版本号
+defaultver = "1.4"
 # 提示字符串
-egstr = 'Eg:python cdn.py -v <Github release version,默认版本号1.3>'
+egstr = 'Eg:python cdn.py -v <Github release version,默认版本号'+defaultver+'>'
 
-def createJsDeliverCDN(ver="1.3"):
+def createJsDeliverCDN(ver=defaultver):
    # github上的用户名、工程名、版本号
    github = "hzdu/publicres@v"+ver
    # jsdelivrCDN使用Github资源的路径
    jsdelivr = "https://cdn.jsdelivr.net/gh/"
 
    # 需要使用CDN链接的文件后缀
-   ext=['.png','.css','gif','js','jpg','bmp','ico','jpeg']
+   ext=['.png','.css','.gif','.js','.jpg','.bmp','.ico','.jpeg']
    for root,dirs,files in os.walk("."):
    	for name in files:
    		if os.path.splitext(name)[1] in ext:
